5efac7bf9ec60d7cd45a5fc1939e8d40c9f44af7
zacchaeusliang <z.liang111@gmail.com>
2018-12-26 17:47:04 -0500

    added buttons

--- app/src/main/java/com/example/zack/mp3play/MainActivity.java
+++ app/src/main/java/com/example/zack/mp3play/MainActivity.java
@@ -6,30 +6,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.media.MediaPlayer;
+
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mPlayer ;
-    int playstate=0 ;
+    MediaPlayer mPlayer;
+    MediaPlayer mPlayer2;
+    MediaPlayer mPlayer3;
+    MediaPlayer mPlayer4;
+    MediaPlayer mPlayer5;
+    MediaPlayer mPlayer6;
+
+    int playstate = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button_id);
+        final Button balanced = findViewById(R.id.balanced);
+        final Button piano = findViewById(R.id.piano);
+        final Button alto = findViewById(R.id.alto);
+        final Button soprano = findViewById(R.id.soprano);
+        final Button tenor = findViewById(R.id.tenor);
+        final Button bass = findViewById(R.id.bass);
 
-        mPlayer = MediaPlayer.create(this, R.raw.bass);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        mPlayer = MediaPlayer.create(this, R.raw.altolamb);
+        mPlayer2 = MediaPlayer.create(this, R.raw.lambpiano);
+        mPlayer3 = MediaPlayer.create(this, R.raw.lambpiano);
+        mPlayer4 = MediaPlayer.create(this, R.raw.lambpiano);
+        mPlayer5 = MediaPlayer.create(this, R.raw.lambpiano);
+        mPlayer6 = MediaPlayer.create(this, R.raw.lambpiano);
+        balanced .setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("hello");
                 // Code here executes on main thread after user presses button
-                if(playstate==0){
+                if (playstate == 0) {
                     mPlayer.seekTo(0);
                     mPlayer.start();
-                    playstate=1;
+                    playstate = 1;
 
-                }else{
+                } else {
                     mPlayer.stop();
-                    playstate=0;
+                    playstate = 0;
                     try {
                         mPlayer.prepare();
 
@@ -40,9 +57,181 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
+            }
+        });
+        piano.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
 
             }
         });
+        tenor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+        soprano.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+        alto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+        alto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+        tenor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+
+        bass.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hello");
+                // Code here executes on main thread after user presses button
+                if (playstate == 0) {
+                    mPlayer2.seekTo(0);
+                    mPlayer2.start();
+                    playstate = 1;
+
+                } else {
+                    mPlayer2.stop();
+                    playstate = 0;
+                    try {
+                        mPlayer2.prepare();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+
+
+
+
     }
 
 //main activity
